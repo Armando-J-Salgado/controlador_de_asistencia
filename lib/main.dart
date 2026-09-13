6ac95d0c7a68f8970fa47f04fed1365449eb0dcf
Nicky2811 <carcamojade@gmail.com>
2026-09-13 17:29:58 -0600

    Registros

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,18 @@ class _MyHomePageState extends State<MyHomePage> {
     Estudiante('Armando', '20245138', true),
     Estudiante('José', '20256589', false),
     Estudiante('Gerardo', '20246378', false),
+    Estudiante('Ana', '20247641', false),
+    Estudiante('Beatriz', '20258732', false),
+    Estudiante('Carlos', '20249853', false),
+    Estudiante('Daniela', '20250964', false),
+    Estudiante('Eduardo', '20251075', false),
+    Estudiante('Fernanda', '20252186', false),
+    Estudiante('Gabriel', '20253297', false),
+    Estudiante('Hector', '20254308', false),
+    Estudiante('Isabel', '20255419', false),
+    Estudiante('Javier', '20256520', false),
+    Estudiante('Karen', '20257631', false),
+    Estudiante('Luis', '20258742', false),
   ];
 
   @override
